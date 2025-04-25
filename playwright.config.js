2367e7c7dc6d86ef7cfa817e6efc3883f9d3289a
Wojciech Niemkowski <wojciech@severalnines.com>
2025-04-25 19:25:44 +0200

    ms -> s timeout + merging report fixes

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ import { defineConfig, devices } from "@playwright/test";
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  globalTimeout: 120,
+  globalTimeout: 120 * 1000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
