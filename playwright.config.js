93d8171ae8d84cf0e70c2f1036a3ba3d29663b52
Wojciech Niemkowski <wojciech@severalnines.com>
2025-04-25 19:21:14 +0200

    shards fix + report path fix

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,7 @@ import { defineConfig, devices } from "@playwright/test";
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  globalTimeout: 120,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -21,7 +22,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
